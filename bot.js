da7a59646c07c12d81b6730be8f64369820ffe8b
fratzorx <alexfratzsantos@outlook.com>
2019-11-15 22:47:34 +0000

    Migrate to a new bot framework

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
@@ -8,16 +8,13 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+var client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Client has been started');
 });
-bot.on('ready', function (evt) {
-    bot.user = "zor";
-    logger.info('Connected');
-    logger.info('Logged in as: ' + bot.user);
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+
+client.login(auth.token);
 
 var months = {1:"Janeiro", 2:"Fevereiro",3:"Março",4:"Abril",5:"Maio",6:"Junho",7:"Julho",8:"Agosto",9:"Setembro",10:"Outubro",11:"Novembro",12:"Dezembro"};
 var dateRegex = /(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/i
@@ -39,26 +36,23 @@ function isEmptyOrSpaces(str) {
     return (str === null || (/^ *$/).test(str) !== null);
 };
 
-bot.on('message', function (user, userId, channelId, message, evt) {
-    if (commandRegex.test(message)) {
+client.on('message', message => {
+    if (commandRegex.test(message.content)) {
         try {
-            var info = message.split(" ");
+            var info = message.content.split(" ");
             var command = info[0].substring(1, info[0].length);
-            if (channelId == testChannelId) {
+            if (message.channel.id == testChannelId) {
                 switch (command) {
                     case 'miss':
-                        var values = tryAndParseDate(info[1], channelId,userId)
+                        var values = tryAndParseDate(message, info[1])
                         if (values.success) {
-                                bot.sendMessage({
-                                    to: channelId,
-                                    message: user + ' vai faltar no dia ' + values.Date[0] + " de " + months[parseInt(values.Date[1])] + " de " + values.Date[2]
-                                });
-                            }
+                            message.channel.send(message.author.username + ' vai faltar no dia ' + values.Date[0] + " de " + months[parseInt(values.Date[1])] + " de " + values.Date[2]);
+                        }
                         break;
                     case 'come':
-                        var values = tryAndParseDate(info[1], channelId,userId)
+                        var values = tryAndParseDate(info[1], message.channel.id, message.author.id)
                         if (values.success) {
-                                bot.sendMessage({
+                                client.sendMessage({
                                     to: channelId,
                                     message: user + ' mudou de planos e pode vir no dia ' + values.Date[0] + " de " + months[parseInt(values.Date[1])] + " de " + values.Date[2]
                                 });
@@ -67,32 +61,32 @@ bot.on('message', function (user, userId, channelId, message, evt) {
                     case 'avail':
                         var values = tryAndParseDate(info[1], channelId,userId)
                         if (values.success) {
-                            bot.sendMessage({
+                            client.sendMessage({
                                 to: channelId,
                                 message: "Há " + 30 + " pessoas disponiveis para dia " + values.Date[0] + " de " + months[parseInt(values.Date[1])] + " de " + values.Date[2]
                             });
                         }
                         break;
                     case 'help':
-                        bot.sendMessage({
+                        client.sendMessage({
                             to: channelId,
                             message: "Available Commands:\n\n !miss dd/MM/yyyy\n !come dd/MM/yyyy\n !avail dd/MM/yyyy\n\n"
                         });
                         break;
                     default:
-                        bot.sendMessage({
+                        client.sendMessage({
                             to: channelId,
                             message: "Command not found!"
                         });
                 }
             } else {
-                bot.sendMessage({
+                client.sendMessage({
                     to: userId,
                     message: "Unauthorized channel to send message"
                 });
             }
         } catch (ex) {
-            bot.sendMessage({
+            client.sendMessage({
                 to: channelId,
                 message: ex
             });
@@ -100,7 +94,7 @@ bot.on('message', function (user, userId, channelId, message, evt) {
     }  
 });
 
-function tryAndParseDate(info, channelId,userId) {
+function tryAndParseDate(message, info) {
     try {
         var date = null
         if (dateRegex.test(info)) {
@@ -108,15 +102,12 @@ function tryAndParseDate(info, channelId,userId) {
             if (checkValidDate(date[0], parseInt(date[1]), date[2])) {
                 return { success: true, Date: date };
             } else {
-                bot.sendMessage({
-                    to: userId,
-                    message: "Não queiras voltar atrás no tempo palhaço"
-                });
+                message.author.send("Não queiras voltar atrás no tempo palhaço");
             }
             if(date!=null)
                 return date;
         }
-        bot.sendMessage({
+        client.sendMessage({
             to: channelId,
             message: "Invalid date!"
         });
